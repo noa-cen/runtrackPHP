4dc6974f91acf92ab7e2a10d666bd50ebb9bf7c5
noa-cen <noa.kreins@laplateforme.io>
2025-01-13 18:14:59 +0100

    valeurs gardées en tableau

--- jour1/job03/index.php
+++ jour1/job03/index.php
@@ -4,23 +4,11 @@ $age = 31;
 $prenom = "Noa";
 $note = 4.5;
 
-$type = Array();
-$type[0] = "booléen";
-$type[1] = "entier";
-$type[2] = "chaîne de caractères";
-$type[3] = "nombre à virgule";
+$type = ["booléen", "entier", "chaîne de caractères", "nombre à virgule"];
 
-$nom = Array();
-$nom[0] = "estEtudiant";
-$nom[1] = "age";
-$nom[2] = "prenom";
-$nom[3] = "note";
+$nom = ["estEtudiant", "age", "prenom", "note"];
 
-$valeur = Array();
-$valeur[0] = $estEtudiant;
-$valeur[1] = $age;
-$valeur[2] = $prenom;
-$valeur[3] = $note;
+$valeur = [$estEtudiant, $age, $prenom, $note];
 
 echo "<table>";
 echo "<tr><th>type</th> <th>nom</th> <th>valeur</th></tr>";
@@ -29,4 +17,5 @@ echo "<tr><td>{$type[1]}</td> <td>{$nom[1]}</td> <td>{$valeur[1]}</td></tr>";
 echo "<tr><td>{$type[2]}</td> <td>{$nom[2]}</td> <td>{$valeur[2]}</td></tr>";
 echo "<tr><td>{$type[3]}</td> <td>{$nom[3]}</td> <td>{$valeur[3]}</td></tr>";
 echo "</table>";
+
 ?>
