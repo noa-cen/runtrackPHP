fc91be8ff6d7f76fd072783fe286ee29bd915a9e
noa-cen <noa.kreins@laplateforme.io>
2025-01-13 18:43:31 +0100

    execice fait en HTML également

--- jour1/job03/index.php
+++ jour1/job03/index.php
@@ -19,3 +19,20 @@ echo "<tr><td>{$type[3]}</td> <td>{$nom[3]}</td> <td>{$valeur[3]}</td></tr>";
 echo "</table>";
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Affichage des recettes</title>
+</head>
+<body>
+    <ul>
+        <?php for ($lines = 0; $lines <= 3; $lines++) : ?>
+            <li><?php echo $recipes[$lines]['title'] . ' (' . $recipes[$lines]['author'] . ')'; ?></li>
+        <?php endfor; ?>
+    </ul>
+</body>
+</html>
