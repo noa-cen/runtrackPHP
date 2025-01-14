da3902a2732ff03fbdf02d35133727c6894ecdea
noa-cen <noa.kreins@laplateforme.io>
2025-01-14 08:50:39 +0100

    enregistrer le bon fichier au bon endroit

--- jour1/job03/index.php
+++ jour1/job03/index.php
@@ -11,28 +11,48 @@ $nom = ["estEtudiant", "age", "prenom", "note"];
 $valeur = [$estEtudiant, $age, $prenom, $note];
 
 echo "<table>";
-echo "<tr><th>type</th> <th>nom</th> <th>valeur</th></tr>";
+echo "<tr><th>Type</th> <th>nom</th> <th>valeur</th></tr>";
 echo "<tr><td>{$type[0]}</td> <td>{$nom[0]}</td> <td>{$valeur[0]}</td></tr>";
 echo "<tr><td>{$type[1]}</td> <td>{$nom[1]}</td> <td>{$valeur[1]}</td></tr>";
 echo "<tr><td>{$type[2]}</td> <td>{$nom[2]}</td> <td>{$valeur[2]}</td></tr>";
 echo "<tr><td>{$type[3]}</td> <td>{$nom[3]}</td> <td>{$valeur[3]}</td></tr>";
 echo "</table>";
-
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Affichage des recettes</title>
-</head>
-<body>
-    <ul>
-        <?php for ($lines = 0; $lines <= 3; $lines++) : ?>
-            <li><?php echo $recipes[$lines]['title'] . ' (' . $recipes[$lines]['author'] . ')'; ?></li>
-        <?php endfor; ?>
-    </ul>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Affichage d'un tableau</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Valeur</th>
+            </tr>
+            <tr>
+                <td><?php echo $type[0] ?></td>
+                <td><?php echo $nom[0] ?></td>
+                <td><?php echo $valeur[0] ?></td>
+            </tr> 
+            <tr>
+                <td><?php echo $type[1] ?></td>
+                <td><?php echo $nom[1] ?></td>
+                <td><?php echo $valeur[1] ?></td>
+            </tr> 
+            <tr>
+                <td><?php echo $type[2] ?></td>
+                <td><?php echo $nom[2] ?></td>
+                <td><?php echo $valeur[2] ?></td>
+            </tr>
+            <tr>
+                <td><?php echo $type[3] ?></td>
+                <td><?php echo $nom[3] ?></td>
+                <td><?php echo $valeur[3] ?></td>
+            </tr> 
+        </table>
+    </body>
 </html>
