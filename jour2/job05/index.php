9ef1b3e511a3866d592a1b9740a2f116dcf90c56
noa-cen <noa.kreins@laplateforme.io>
2025-01-14 12:27:37 +0100

    job05

--- jour2/job05/index.php
+++ jour2/job05/index.php
@@ -1,15 +1,26 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Job 1</title>
+        <title>Job 5</title>
     </head>
     <body>
-      
+    <?php 
+        $estPremier = False;
+        for ($x=2; $x<=1000; $x++) {
+            $nbDiviseurs = 0;
+            for ($n=1; $n<=$x; $n++) {
+                if ($x % $n == 0) {
+                    $nbDiviseurs++;
+                }
+            }
+            if ($nbDiviseurs == 2) {
+                $estPremier = True;
+                echo $x;
+                echo "<br>";
+            }
+        };
+    ?>
     </body>
 </html>
