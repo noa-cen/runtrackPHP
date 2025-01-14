aaf8197a624d6a7192d4140f831dd5c34af5de2f
noa-cen <noa.kreins@laplateforme.io>
2025-01-14 09:39:35 +0100

    job02

--- jour2/job02/index.php
+++ jour2/job02/index.php
@@ -1,15 +1,33 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Job 1</title>
+        <title>Job 2</title>
     </head>
     <body>
-      
+    <?php 
+        $x = 0;
+        while ($x <= 1337) {
+            switch ($x) {
+                case 26:
+                    echo "<br>";
+                    break;
+                case 37:
+                    echo "<br>";
+                    break;
+                case 88:
+                    echo "<br>";
+                    break;
+                case 1111:
+                    echo "<br>";
+                    break;
+                default:
+                    echo $x;
+                    echo "<br>";
+            }
+            $x++;
+        }
+    ?>
     </body>
 </html>
