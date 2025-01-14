c8ad9428f2f4dfb73aa9957238b84ea9a7414302
noa-cen <noa.kreins@laplateforme.io>
2025-01-14 10:05:17 +0100

    job04

--- jour2/job04/index.php
+++ jour2/job04/index.php
@@ -1,15 +1,35 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Job 1</title>
+        <title>Job 4</title>
     </head>
     <body>
-      
+    <?php 
+        $x = 1;
+        while ($x <= 100) {
+            if ($x % 3 == 0 && $x % 5 !== 0) {
+                echo "Fizz";
+                echo "<br>";
+                $x++;
+            }
+            elseif ($x % 5 == 0 && $x % 3 !== 0) {
+                echo "Buzz";
+                echo "<br>";
+                $x++;
+            }
+            elseif ($x % 3 == 0 && $x % 5 == 0) {
+                echo "FizzBuzz";
+                echo "<br>";
+                $x++;
+            }
+            else {
+                echo $x;
+                echo "<br>";
+                $x++;
+            }
+        }
+    ?>
     </body>
 </html>
