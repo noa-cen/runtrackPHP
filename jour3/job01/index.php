9e9b4e9a0c4b7354085f2b674bfee772446868a5
noa-cen <noa.kreins@laplateforme.io>
2025-01-15 11:03:29 +0100

    job01

---
+++ jour3/job01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Job 5</title>
+    </head>
+    <body>
+    <?php 
+        $nombres = [200, 204, 173, 98, 171, 404, 459];
+        foreach ($nombres as $x) {
+            if ($x % 2 == 0) {
+                echo "".$x." est paire <br />";
+            }
+            else {
+                echo "".$x." est impaire <br />";
+            }
+        };
+    ?>
+    </body>
+</html>
