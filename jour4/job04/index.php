58b20d42dc4832d601ca00ccca97a547d8db83d7
noa-cen <noa.kreins@laplateforme.io>
2025-01-16 18:14:56 +0100

    job04

---
+++ jour4/job04/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 4</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            text-align: left;
+            padding: 10px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="surname">Prénom :</label>
+        <input type="text" id="surname" name="Prénom" placeholder="Votre prénom"><br><br>
+
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="Nom" placeholder="Votre nom"><br><br>
+
+        <label for="age">Age :</label>
+        <input type="text" id="age" name="Age" placeholder="Votre âge"><br><br>
+
+        <button type="submit">Envoyer</button>
+    </form>
+    <table>
+        <tr>
+            <?php 
+            foreach ($_POST as $name => $value) {
+                echo "<th>".$name."</th>";
+            }
+            ?>
+        </tr>
+        <tr>
+            <?php 
+            foreach ($_POST as $name => $value) {
+                echo "<td>".$value."</td>";
+            }
+            ?>
+        </tr>
+    </table>
+
+</body>
+</html>
