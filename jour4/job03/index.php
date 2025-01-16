9bbd0995ae7fe24fc1dc0e26523325913e003e06
noa-cen <noa.kreins@laplateforme.io>
2025-01-16 18:12:37 +0100

    job03

--- jour4/job03/index.php
+++ jour4/job03/index.php
@@ -1,30 +1,34 @@
-<?php 
-$compteur = 0;
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    foreach ($_POST as $name => $value) {
-        $compteur++;
-    };
-};
-?>
-
-
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Job 3</title>
-    </head>
-    <body>
-        <h1>Ce formulaire récupère <?php echo $compteur; ?> arguments POST.</h1>
-        <form action="/index.php" method="POST">
-            <label for="prenom">Prénom:</label><br>
-            <input type="text" id="prenom" name="prenom" value=""><br>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 3</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="surname">Prénom :</label>
+        <input type="text" id="surname" name="Prénom" placeholder="Votre prénom"><br><br>
 
-            <label for="nom">Nom:</label><br>
-            <input type="text" id="nom" name="nom" value=""><br><br>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="Nom" placeholder="Votre nom"><br><br>
 
-            <input type="submit" value="Submit">
-        </form> 
-    </body>
+        <label for="age">Age :</label>
+        <input type="text" id="age" name="Age" placeholder="Votre âge"><br><br>
+
+        <button type="submit">Envoyer</button>
+    </form>
+
+    <?php
+    $compteur = 0;
+
+    foreach ($_POST as $key => $value) {
+        if ($value !== "") {
+            $compteur++;
+        }
+    };
+    echo "Le nombre d'arguments POST est ".$compteur;
+
+?>
+</body>
 </html>
