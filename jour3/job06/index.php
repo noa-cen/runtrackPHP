b6ff824a9460d0aadd4a9c9e799f79200903be05
noa-cen <noa.kreins@laplateforme.io>
2025-01-16 09:05:42 +0100

    job06

--- jour3/job06/index.php
+++ jour3/job06/index.php
@@ -3,18 +3,20 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Job 5</title>
+        <title>Job 6</title>
     </head>
     <body>
-    <?php 
-        $str = "Les choses que l'on Possède finissent par nous posséder";
-        $i = 0;
-        while(isset($str[$i])) {
-            $i++;
-        };
-        for ($j = $i; $j >= 0; $j--) {
-            echo $str[$j];
-        };
+    <?php
+        $str = "Les choses que l'on Possede finissent par nous posseder";
+        $inverse = " ";
+        $longeur= 0;
+        while(isset($str[$longeur]) != null){
+            $longeur++;
+        }
+        for ($i = $longeur -1; $i > 0 ; $i-- ) {
+            $inverse .= $str[$i];
+        }
+        echo $inverse;
     ?>
     
     </body>
