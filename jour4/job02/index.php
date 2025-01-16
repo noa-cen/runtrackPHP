2fed5b07f600e76ffef1e97761561d30ffacbd8a
noa-cen <noa.kreins@laplateforme.io>
2025-01-16 18:10:56 +0100

    job02

--- jour4/job02/index.php
+++ jour4/job02/index.php
@@ -1,43 +1,54 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Job 2</title>
-    </head>
-    <body>
-        <form action="/index.php" method="GET">
-            <label for="prenom">Prénom:</label><br>
-            <input type="text" id="prenom" name="prenom" value=""><br>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 1</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            text-align: left;
+            padding: 10px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <form action="" method="get">
+        <label for="surname">Prénom :</label>
+        <input type="text" id="surname" name="Prénom" placeholder="Votre prénom"><br><br>
 
-            <label for="nom">Nom:</label><br>
-            <input type="text" id="nom" name="nom" value=""><br>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="Nom" placeholder="Votre nom"><br><br>
 
-            <label for="age">Age:</label><br>
-            <input type="text" id="age" name="age" value=""><br><br>
+        <label for="age">Age :</label>
+        <input type="text" id="age" name="Age" placeholder="Votre âge"><br><br>
 
-            <input type="submit" value="Submit">
-        </form> 
-        <br>
-        <table>
-            <tr>
-                <?php 
-                foreach ($_GET as $name => $value) {
-                    echo "<th>".$name."</th>";
-                };
-                ?>
-            </tr>
-            <tr>
+        <button type="submit">Envoyer</button>
+    </form>
+    <table>
+        <tr>
             <?php 
-                foreach ($_GET as $name => $value) {
-                    echo "<td>".$value."</td>";
-                };
-                ?>
-            </tr>
-        </table>
-        <style>
-            table, th, td {border: 1px solid #000;
-            border-collapse: collapse;}
-        </style>
-    </body>
+            foreach ($_GET as $name => $value) {
+                echo "<th>".$name."</th>";
+            }
+            ?>
+        </tr>
+        <tr>
+            <?php 
+            foreach ($_GET as $name => $value) {
+                echo "<td>".$value."</td>";
+            }
+            ?>
+        </tr>
+    </table>
+
+</body>
 </html>
