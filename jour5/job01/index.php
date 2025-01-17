2a3498c3c995b17a05d2c49eab7e0fb1b46dca20
noa-cen <noa.kreins@laplateforme.io>
2025-01-17 10:28:14 +0100

    job01

--- jour5/job01/index.php
+++ jour5/job01/index.php
@@ -3,22 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job 2</title>
-    <style>
-        table {
-            width: 50%;
-            border-collapse: collapse;
-            margin: 20px auto;
-        }
-        th, td {
-            border: 1px solid #ddd;
-            text-align: left;
-            padding: 10px;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
+    <title>Job 1</title>
 </head>
 <body>
     <?php 
