dfa898632a14d83606820f7a0d810af3a188b31f
noa-cen <noa.kreins@laplateforme.io>
2025-01-17 10:34:01 +0100

    job03

---
+++ jour5/job03/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 1</title>
+</head>
+<body>
+    <?php 
+        function getHello() {
+            return "Hello La Plateforme !";
+        }
+    ?>
+    <h1><?php echo getHello() ?></h1>
+</body>
+</html>
